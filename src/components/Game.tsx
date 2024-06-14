b6540547dcbfb0db0c38ca05413c03fad5603f5f
hoges9898 <ben@3dpixel.au>
2024-06-14 15:04:11 +1000

    initial commit

---
+++ src/components/Game.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.div`
+  padding: 10px;
+`;
+
+function Game() {
+  return <Title>Game</Title>;
+}
+
+export default Game;
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.header`
+  padding: 10px;
+  font-size: 32px;
+`;
+
+function Header() {
+  return <Title>Best number match game... ever</Title>;
+}
+
+export default Header;
